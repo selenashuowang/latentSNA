26b6dfeed8990d71e0541534ed233ee5b40d2fe4
Selena Wang <selenashuowang@gmail.com>
2024-06-17 12:49:25 -0400

    xx

---
+++ R/X.R
@@ -0,0 +1,10 @@
+#' @title Simulated a list of V by V connectivity data
+#'
+#' @description The dataset contains a list of V by V connectivity data
+#'
+#' @details a list of V by V connectivity data
+#'
+#' @docType data
+#' @keywords datasets
+#' @format List including a list of weighted adjacency matrices of length N.
+"X"
---
+++ R/Y.R
@@ -0,0 +1,10 @@
+#' @title Simulated a matrix of N by P individual outcomes data
+#'
+#' @description The dataset contains a matrix of N by P individual outcomes data
+#'
+#' @details a matrix of N by P individual outcomes data
+#'
+#' @docType data
+#' @keywords datasets
+#' @format List including a matrix of N by P individual outcomes data about N individuals and P variables measuring a latent construct
+"Y"
